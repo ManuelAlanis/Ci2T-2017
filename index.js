2d0e25d5de00fffc9482437b80110fe1ccfb560e
Manuel Alanis <manuel.11.11@hotmail.es>
2016-09-20 10:58:03 -0700

    Enrutado

---
+++ index.js
@@ -0,0 +1,42 @@
+/**
+* ci2t Module
+*
+* Description
+*/
+var app=angular.module('ci2t', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "slider.html"
+    })
+    .when("/home", {
+        templateUrl : "slider.html"
+    })
+    .when("/contact", {
+        templateUrl : "contacto.html"
+    });
+});
+
+app.controller('controller1', function($scope){
+	
+
+	$scope.nombre="Erick"
+
+	$scope.event=function  () {
+		alert($scope.nombre)
+	}
+
+	$scope.array=[
+		{name:"name1"},
+		{name:"name2"},
+		{name:"name4"},
+		{name:"name5"},
+		{name:"name6"},
+		{name:"name5"}
+	]
+
+	$scope.json=function  (argument) {
+		alert(argument);
+	}
+});
